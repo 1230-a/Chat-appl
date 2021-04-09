390a69de08b3cbe8e624069bafa2dd008861eabf
Nana  BAKALAFOUA <nbakalafoua@gmail.com>
2021-04-09 23:21:31 +0200

    Adding the frontend

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,13 @@
 const express = require('express')
 const mongoose =  require('mongoose');
 const app = express()
+const cors = require('cors')//helps to communicate with teh api that we create
 
 const UsersModel = require("./models/Users")
 
 //Receive information from frontend in json format
-app.use(express.json())
+app.use(express.json());
+app.use(cors())
 
 //Setting up the connection to the database we are trying to talk to
 mongoose.connect("mongodb+srv://admin:P@ssw0rd123@cluster0.upvyl.mongodb.net/users?retryWrites=true&w=majority", {
@@ -15,10 +17,14 @@ mongoose.connect("mongodb+srv://admin:P@ssw0rd123@cluster0.upvyl.mongodb.net/use
 })
 
 //Inserting data in the database table
-app.get('/', async (req, res) => {
+app.post('/create_user', async (req, res) => {
+
+    const username = req.body.username;
+    const password = req.body.password
+
     const utilisateur = new UsersModel ({
-        username : "user2",
-        password : "Password123"
+        username : username,
+        password : password
     });
 
     try {
@@ -28,6 +34,15 @@ app.get('/', async (req, res) => {
         console.log(err)
     }
     
+})
+
+//Retrieveing data from the database to compare
+app.get('/login', async (req, res) => {
+    UsersModel.find({ $where: {username : "" }},(err, result) => {
+        if (err) {
+            res.send(err)
+        }
+    } )
 })
 
 //Setting up the port that the backend server will listen to
